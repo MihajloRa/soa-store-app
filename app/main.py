dfb03bd0b2712255f2d549386be66f0982dcde20
MihajloRa <42686887+MihajloRa@users.noreply.github.com>
2022-05-10 12:54:41 +0200

    --

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import get_session
 from app.models import Product, ProductReadDTO, Category, ProductUpdateDTO
-from project.app.models import ProductCreateDTO, ProductReadDTO
+from app.models import ProductCreateDTO, ProductReadDTO
 
 app = FastAPI()
 
